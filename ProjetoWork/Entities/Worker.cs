dfed2af74e400eb1b1d22e29d3acbce2042b4d94
PedroBoscachi <pedroboscachi@gmail.com>
2022-01-05 20:31:28 -0300

    Primeiro commit: criado entidades e enum

---
+++ ProjetoWork/Entities/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ProjetoWork.Entities.Enums;
+using ProjetoWork.Entities;
+
+
+namespace ProjetoWork.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public WorkerLevel Level { get; set; }
+
+        void AddContract(HourContrat contract)
+        {
+
+        }
+    }
+}
---
+++ ProjetoWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
